ea2a8da5a3ad46a153ca8896fbcce718d1079a36
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-12-20 23:20:12 +0530

    update demo-03

--- demo-03/index.js
+++ demo-03/index.js
@@ -1,1 +1,9 @@
 console.log("Hello, world!");
+var x = 10;
+console.log(x);
+var y = "Hello, world!";
+console.log(y);
+var z = 10;
+console.log(z);
+z = "Hello, world!";
+console.log(z);
--- demo-03/index.ts
+++ demo-03/index.ts
@@ -1,1 +1,12 @@
 console.log("Hello, world!");
+
+let x = 10;
+console.log(x);
+
+let y: String = "Hello, world!";
+console.log(y);
+
+let z: any = 10;
+console.log(z);
+z = "Hello, world!";
+console.log(z);
